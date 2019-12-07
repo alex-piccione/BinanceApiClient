107810249cb1def8a1f73c36d5f187cf643ffabd
Alessandro <alessandro.piccione.75@gmail.com>
2019-12-07 19:33:12 +0000

    try add confirm override parameter in post-build script

--- IntegrationTests/post-build.cmd
+++ IntegrationTests/post-build.cmd
@@ -3,4 +3,4 @@ set ProjectDir=%~1
 set TargetDir=%~2
 
 ::copy "%ProjectDir%settings.secret.yaml" "%TargetDir%"
-IF EXIST "%ProjectDir%settings.secret.yaml" (copy "%ProjectDir%settings.secret.yaml" "%TargetDir%settings.yaml")
+IF EXIST "%ProjectDir%settings.secret.yaml" (copy /Y "%ProjectDir%settings.secret.yaml" "%TargetDir%settings.yaml")
