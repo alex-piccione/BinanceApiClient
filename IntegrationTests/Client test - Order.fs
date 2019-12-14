f17be193c9ea2dc6424e2db0a268c0af6401352f
Alessandro <alessandro.piccione.75@gmail.com>
2019-12-14 17:23:31 +0000

    Correct Create Order function and add tests

--- IntegrationTests/Client test - Order.fs
+++ IntegrationTests/Client test - Order.fs
@@ -25,6 +25,40 @@ type ClientTest_Order () =
         response.Id |> should be (greaterThan 0)
         response.Price |> should be (greaterThan 0)
 
+    //[<Test; Category("AFFECT_BALANCE")>]
+    //member __.``CreateMarketOrder Buy BTC with 20 XRP`` () =
+
+    // the pair BTC/XRP does not exists, this order must be executed on the other way (Sell XRP for BTC)
+
+        //let pair = CurrencyPair("btc", "xrp") invalid symbol
+        //let pair = CurrencyPair("xrp", "btc")
+        //let price = 0.00003016m // XRP/BTC
+        //let amount = price * 20m // pay 20 XRP
+        //let response = client.CreateMarketOrder(pair, OrderSide.Sell, amount)
+
+        //response |> should not' (be null)
+        //if not response.IsSuccess then failwith response.Error
+
+        //response.IsSuccess |> should equal true
+        //response.Id |> should be (greaterThan 0)
+        //response.Price |> should be (greaterThan 0)
+
+    [<Test; Category("AFFECT_BALANCE")>]
+    member __.``CreateMarketOrder Sell 20 XRP for BTC`` () =
+        //let pair = CurrencyPair("btc", "xrp") invalid symbol
+        let pair = CurrencyPair("xrp", "btc")
+        //let price = 0.00003016m // XRP/BTC
+        //let amount = price * 20m // pay 20 XRP
+        let amount = 20m  // XRP
+        let response = client.CreateMarketOrder(pair, OrderSide.Sell, amount)
+
+        response |> should not' (be null)
+        if not response.IsSuccess then failwith response.Error
+
+        response.IsSuccess |> should equal true
+        response.Id |> should be (greaterThan 0)
+        //response.Price |> should be (greaterThan 0) it returns "0"
+
 
     //[<Test; Category("AFFECT_BALANCE")>]
     //member __.``CreateLimitOrder`` () =
