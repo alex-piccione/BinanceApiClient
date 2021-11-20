354c8ea80eedb89f26b9b0598c79bd253b1babfa
Alex <alessandro.piccione.75@gmail.com>
2021-11-20 18:20:42 +0000

    remove error when secrets is not found to avoid error on CI

--- IntegrationTests/settings.fs
+++ IntegrationTests/settings.fs
@@ -9,13 +9,9 @@ let mutable withdrawalAddress = ""
 let readSettings () = 
     let configuration = ConfigurationBuilder().AddUserSecrets("Alex75.BinanceApiClient-79043534-af9b-475e-9b77-7bc83c77c156").Build()
 
-    let readValue field = match configuration.[field] with
-                          | value when String.IsNullOrEmpty(value) -> failwith $"Cannot find \"{field}\" is secret"
-                          | value -> value
-
-    withdrawalAddress <- readValue("withdrawal address")
-    let publicKey = readValue("public key")
-    let secretKey = readValue("secret key")
+    withdrawalAddress <- configuration.["withdrawal address"]
+    let publicKey = configuration.["public key"]
+    let secretKey = configuration.["secret key"]
  
     { TickerCacheDuration=TimeSpan.FromSeconds(60.); PublicKey=publicKey; SecretKey=secretKey; }
 
